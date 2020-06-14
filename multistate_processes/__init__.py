2297a0f621a4eab1faf132b5f0e3ec5a04a02feb
Matej Kerekrety <matej.kerekrety@gmail.com>
2020-06-14 08:38:06 +0100

    v0.0.2

---
+++ multistate_processes/__init__.py
@@ -0,0 +1,4 @@
+from multistate_processes.process import MultistateProcess  # noqa
+from multistate_processes.simulate import SimulateProcess  # noqa
+from multistate_processes.meanfield import MeanField  # noqa
+from multistate_processes.calculate import CalculateProcess  # noqa
